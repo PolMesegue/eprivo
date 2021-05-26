3b6022b75fde2ba856d825c1c688466c8eb005ae
PolMesegue <pol@mesegue.me>
2021-05-26 15:42:39 +0200

    map with tooltip

--- domain.php
+++ domain.php
@@ -8,7 +8,7 @@ $sql = "SELECT url.country_code AS alpha2, count(url.country_code) AS reps FROM 
 
 $tmpfname = tempnam("/var/www/html/data/", 'orm');
 $handle = fopen($tmpfname, "w");
-fwrite($handle, "code,reps" . PHP_EOL);
+fwrite($handle, "id,reps" . PHP_EOL);
 
 
 if ($stmt = mysqli_prepare($link, $sql)) {
@@ -39,6 +39,81 @@ $pieces = explode('/data', $tmpfname);
 <!DOCTYPE html>
 <html lang="en">
 
+<style>
+    .names {
+        fill: none;
+        stroke: #fff;
+        stroke-linejoin: round;
+    }
+
+    /* Tooltip CSS */
+    .d3-tip {
+        line-height: 1.5;
+        font-weight: 400;
+        font-family: "avenir next", Arial, sans-serif;
+        padding: 6px;
+        background: rgba(0, 0, 0, 0.6);
+        color: #FFA500;
+        border-radius: 1px;
+        pointer-events: none;
+    }
+
+    /* Creates a small triangle extender for the tooltip */
+    .d3-tip:after {
+        box-sizing: border-box;
+        display: inline;
+        font-size: 8px;
+        width: 100%;
+        line-height: 1.5;
+        color: rgba(0, 0, 0, 0.6);
+        position: absolute;
+        pointer-events: none;
+
+    }
+
+    /* Northward tooltips */
+    .d3-tip.n:after {
+        content: "\25BC";
+        margin: -1px 0 0 0;
+        top: 100%;
+        left: 0;
+        text-align: center;
+    }
+
+    /* Eastward tooltips */
+    .d3-tip.e:after {
+        content: "\25C0";
+        margin: -4px 0 0 0;
+        top: 50%;
+        left: -8px;
+    }
+
+    /* Southward tooltips */
+    .d3-tip.s:after {
+        content: "\25B2";
+        margin: 0 0 1px 0;
+        top: -8px;
+        left: 0;
+        text-align: center;
+    }
+
+    /* Westward tooltips */
+    .d3-tip.w:after {
+        content: "\25B6";
+        margin: -4px 0 0 -1px;
+        top: 50%;
+        left: 100%;
+    }
+
+    /*    text{
+      pointer-events:none;
+    }*/
+
+    .details {
+        color: white;
+    }
+</style>
+
 
 <head>
     <meta charset="UTF-8">
@@ -48,12 +123,10 @@ $pieces = explode('/data', $tmpfname);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/682550c010.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
-
-
-    <script src="http://d3js.org/d3.v4.js"></script>
-    <script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
-    <script src="https://d3js.org/d3-geo-projection.v2.min.js"></script>
-
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+    <script src="https://d3js.org/queue.v1.min.js"></script>
+    <script src="https://d3js.org/topojson.v1.min.js"></script>
+    <script src="d3-tip.js"></script>
 </head>
 
 <body onload="showGraph()">
@@ -87,54 +160,112 @@ $pieces = explode('/data', $tmpfname);
                 <div class="tab-pane fade show active" id="nav-graph" role="tabpanel" aria-labelledby="nav-graph-tab">
                 </div>
                 <div class="tab-pane fade" id="nav-map" role="tabpanel" aria-labelledby="nav-map-tab">
-                    <svg width="600" height="450"></svg>
+                    <svg width="920" height="600"> </svg>
+
                     <script>
-                        // The svg
-                        var svg = d3.select("svg"),
-                            width = +svg.attr("width"),
-                            height = +svg.attr("height");
+                        var format = d3.format(",");
 
-                        // Map and projection
-                        var path = d3.geoPath();
-                        var projection = d3.geoMercator()
-                            .scale(70)
-                            .center([0, 20])
-                            .translate([width / 2, height / 2]);
-
-                        // Data and color scale
-                        var data = d3.map();
-                        var colorScale = d3.scaleThreshold()
-                            .domain([1, 4, 8, 13, 26, 31])
-                            .range(d3.schemeBlues[7]);
-
-                        // Load external data and boot
-                        d3.queue()
-                            .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
-                            .defer(d3.csv, <?php echo "\"/data" . $pieces[1] . "\""; ?>, function(d) {
-                                data.set(d.code, +d.reps);
+                        // Set tooltips
+                        var tip = d3.tip()
+                            .attr('class', 'd3-tip')
+                            .offset([-10, 0])
+                            .html(function(d) {
+                                return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Reps: </strong><span class='details'>" + format(d.reps) + "</span>";
                             })
+
+                        var margin = {
+                                top: 50,
+                                right: 0,
+                                bottom: 0,
+                                left: 200
+                            },
+                            width = 920 - margin.left - margin.right,
+                            height = 600 - margin.top - margin.bottom;
+
+                        var color = d3.scaleThreshold()
+                            .domain([1, 4, 8, 13, 26, 31])
+                            .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)"]);
+
+                        var path = d3.geoPath();
+
+                        var svg = d3.select("svg")
+                            //.append("svg")
+                            .attr("width", width)
+                            .attr("height", height)
+                            .append('g')
+                            .attr('class', 'map');
+
+                        var projection = d3.geoMercator()
+                            .scale(100)
+                            //.center([0, 20])
+                            .translate([width / 2, height / 1.5]);
+
+                        var path = d3.geoPath().projection(projection);
+
+                        svg.call(tip);
+
+                        queue()
+                            .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
+                            .defer(d3.csv, <?php echo "\"/data" . $pieces[1] . "\""; ?>)
                             .await(ready);
 
-                        function ready(error, topo) {
+                        function ready(error, data, reps) {
+                            var populationById = {};
 
-                            // Draw the map
+                            reps.forEach(function(d) {
+                                populationById[d.id] = +d.reps;
+                            });
+                            data.features.forEach(function(d) {
+
+                                d.reps = populationById[d.id]
+
+                                if (d.reps === undefined) {
+                                    d.reps = 0;
+                                }
+
+                            });
+
                             svg.append("g")
+                                .attr("class", "countries")
                                 .selectAll("path")
-                                .data(topo.features)
-                                .enter()
-                                .append("path")
-                                // draw each country
-                                .attr("d", d3.geoPath()
-                                    .projection(projection)
-                                )
-                                // set the color of each country
-                                .attr("fill", function(d) {
-                                    d.total = data.get(d.id) || 0;
-                                    return colorScale(d.total);
+                                .data(data.features)
+                                .enter().append("path")
+                                .attr("d", path)
+                                .style("fill", function(d) {
+                                    return color(populationById[d.id]);
+                                })
+                                .style('stroke', 'white')
+                                .style('stroke-width', 1.5)
+                                .style("opacity", 0.8)
+                                // tooltips
+                                .style("stroke", "white")
+                                .style('stroke-width', 0.3)
+                                .on('mouseover', function(d) {
+                                    tip.show(d);
+
+                                    d3.select(this)
+                                        .style("opacity", 1)
+                                        .style("stroke", "white")
+                                        .style("stroke-width", 3);
+                                })
+                                .on('mouseout', function(d) {
+                                    tip.hide(d);
+
+                                    d3.select(this)
+                                        .style("opacity", 0.8)
+                                        .style("stroke", "white")
+                                        .style("stroke-width", 0.3);
                                 });
+
+                            svg.append("path")
+                                .datum(topojson.mesh(data.features, function(a, b) {
+                                    return a.id !== b.id;
+                                }))
+                                // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
+                                .attr("class", "names")
+                                .attr("d", path);
                         }
                     </script>
-
                 </div>
             </div>
         </div>
