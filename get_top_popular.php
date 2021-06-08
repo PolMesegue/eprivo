4d60b3a819500b4c890398f7c7563ad519540190
PolMesegue <pol@mesegue.me>
2021-06-08 17:27:55 +0200

    new tops

--- get_top_popular.php
+++ get_top_popular.php
@@ -4,7 +4,7 @@
 require_once "config.php";
 
 
-$sql = "select alexa_rank, name, update_timestamp from domain where id <= 20";
+$sql = "SELECT alexa_rank, domain_name, SUM(count_trackings) AS intrusion_lvl FROM (select domain.alexa_rank AS alexa_rank, tracking.name, domain.name AS domain_name, 0.5 * ((10+(count(tracking.name) * tracking.intrusion_level)) - ABS (10-(count(tracking.name) * tracking.intrusion_level))) AS count_trackings FROM domain JOIN domain_url ON domain.id = domain_url.domain_id JOIN url ON url.id = domain_url.url_id LEFT JOIN url_tracking ON url_tracking.url_id = url.id LEFT JOIN tracking ON tracking.id = url_tracking.tracking_id WHERE tracking.id IS NOT NULL GROUP BY domain.alexa_rank, tracking.name, domain.name, tracking.intrusion_level UNION SELECT domain.alexa_rank AS alexa_rank, tracking.name, domain.name, 0.5 * ((10+(count(tracking.name) * tracking.intrusion_level)) - ABS (10-(count(tracking.name) * tracking.intrusion_level))) AS count_trackings FROM domain JOIN domain_url ON domain.id = domain_url.domain_id JOIN url ON url.id = domain_url.url_id JOIN resource ON resource.id = url.resource_id LEFT JOIN resource_tracking ON resource_tracking.resource_id = resource.id LEFT JOIN tracking ON tracking.id = resource_tracking.tracking_id WHERE tracking.id IS NOT NULL GROUP BY domain.alexa_rank, tracking.name, domain.name, tracking.intrusion_level) AS tracking GROUP BY alexa_rank, domain_name ORDER BY alexa_rank ASC limit 20";
 $result = mysqli_query($link, $sql);
 
 echo "<table class=\"table table-hover text-center\">";
@@ -15,11 +15,11 @@ echo "<th style=\"background: #557bce;position: sticky;top: 0px;\">Intrusion Lev
 echo "<th style=\"background: #557bce;position: sticky;top: 0px;\"> </th>";
 echo "</tr>";
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr id = {$row['name']} onclick=\"updateTrackingGrid(this.id); updateWebGrid(this.id);\">";
+    echo "<tr id = {$row['domain_name']} onclick=\"updateTrackingGrid(this.id); updateWebGrid(this.id);\">";
     echo "<td> {$row['alexa_rank']} </td>";
-    echo "<td> {$row['name']}</td>";
-    echo "<td> {$row['update_timestamp']} </td>";
-    echo "<td> <a href=\"domain.php?domain_url={$row['name']}\"><i class=\"far fa-plus-square\"></i></a></td>";
+    echo "<td> {$row['domain_name']}</td>";
+    echo "<td> {$row['intrusion_lvl']} </td>";
+    echo "<td> <a href=\"domain.php?domain_url={$row['domain_name']}\"><i class=\"far fa-plus-square\"></i></a></td>";
     echo "</tr>";
 }
 
@@ -27,6 +27,7 @@ mysqli_free_result($result);
 
 mysqli_close($link);
 echo "</table>";
+
 
 ?>
 
