eb06956cd6fac63ee5f0763e9433d552fef64764
PolMesegue <pol@mesegue.me>
2021-06-09 19:53:05 +0200

    ja va sotademanda

--- domain_analizer.php
+++ domain_analizer.php
@@ -101,16 +101,6 @@ if (checkdnsrr($domain, "A")) {
 
     }
 
-
-    /*
-        var myVar = setInterval(myTimer, 1000);
-
-        function myTimer() {
-            var d = new Date();
-            var t = d.toLocaleTimeString();
-            document.getElementById("demo").innerHTML = t;
-        }
-        */
 </script>
 
 </html>
