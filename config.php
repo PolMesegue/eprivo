06f9a8d2378eed179f480fdc0efcebbe6116b2b3
PolMesegue <pol@mesegue.me>
2021-06-09 23:51:05 +0200

    fixed style

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'ormdb');
 define('DB_PASSWORD', 'e7rzRQRdw!9rSr6#7mu!jyUAGEZU9r');
 define('DB_NAME', 'ORM');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
